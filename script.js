d08c2de9e5a0b6f68525862d259d33bc37500b8b
ThanPhallapanharithy <145187389+ThanPhallapanharithy@users.noreply.github.com>
2023-09-16 16:07:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const logregBox = document .querySelector('.logreg-box');
+const loginLink = document .querySelector('.login-Link');
+const registerLink = document .querySelector('.register-link');
+
+registerLink.addEventListener('click',()=>{
+    logregBox.classList.add('active');                          
+});
+
+loginLink.addEventListener('click',()=>{
+    logregBox.classList.remove ('active');
+});
+signin
+Link.addEventListener('click',()=>{
+    logregBox.classList.remove ('active');
+})
+
